f586f35d99af0c5697be9bc92036bc209e7382a4
hoangphuc <hoangphucnb97@gmail.com>
2022-12-09 14:21:24 +0700

    First commit

---
+++ contracts/DocumentTracker.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+
+contract DocumentTracker is AccessControlUpgradeable, ERC721Upgradeable, ERC721EnumerableUpgradeable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    function initialize() public initializer {
+        __ERC721_init("DocumentTracker", "DOT");
+        __ERC721Enumerable_init();
+        __AccessControl_init();
+
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(
+            ERC721Upgradeable,
+            ERC721EnumerableUpgradeable,
+            AccessControlUpgradeable
+        ) returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override (
+            ERC721Upgradeable,
+            ERC721EnumerableUpgradeable
+        )
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+}
