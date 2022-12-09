678f0e5955d24cbb8550f3cd8a999ad352015d8c
hoangphuc <hoangphucnb97@gmail.com>
2022-12-09 15:11:58 +0700

    Add saveContract and getContract method

--- test/utils.ts
+++ test/utils.ts
@@ -6,3 +6,25 @@ export function getHashContentOfFile(fileName: string): string {
     const content = fs.readFileSync(path.join(__dirname, './resources', fileName), 'utf8');
     return ethers.utils.keccak256(Buffer.from(content));
 }
+
+export async function saveContract(network: string, contract: string, address: string) {
+    const addresses = await getContracts();
+    addresses[network] = addresses[network] || {};
+    addresses[network][contract] = address;
+    fs.writeFileSync(path.join(__dirname, '../data/contract-addresses.json'),
+                                JSON.stringify(addresses, null, "    "));
+}
+
+export function getContracts(): any {
+    let json;
+    try {
+        json = fs.readFileSync(path.join(__dirname,'../data/contract-addresses.json'), 'utf-8');
+    } catch {
+        json = '{}';
+    }   
+    return JSON.parse(json);
+}
+
+export function sleep(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
