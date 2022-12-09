0ba335b3b566a34a273db9ca89923bef8b2a234d
hoangphuc <hoangphucnb97@gmail.com>
2022-12-09 14:41:33 +0700

    Add Mint function

--- contracts/DocumentTracker.sol
+++ contracts/DocumentTracker.sol
@@ -5,13 +5,36 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract DocumentTracker is AccessControlUpgradeable, ERC721Upgradeable, ERC721EnumerableUpgradeable {
+contract DocumentTrackerContract is AccessControlUpgradeable, ERC721Upgradeable, ERC721EnumerableUpgradeable {
+    struct DocumentTracker {
+        bytes32 hashContent;
+        address orgOwner;
+    }
+
+    struct HashContentIndex {
+        bool isExist;
+        uint256 tokenId;
+    }
+
+    DocumentTracker[] public trackers;
+    uint256 public trackerCount;
+    mapping(bytes32 => HashContentIndex) public contentIndex;
+
     function initialize() public initializer {
         __ERC721_init("DocumentTracker", "DOT");
         __ERC721Enumerable_init();
         __AccessControl_init();
 
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function mint(bytes32 hashContent) public {
+        HashContentIndex memory index = contentIndex[hashContent];
+        require(!index.isExist, "Content already uploaded");
+        trackers.push(DocumentTracker(hashContent, msg.sender));
+        contentIndex[hashContent] = HashContentIndex(true, trackerCount);
+        _mint(msg.sender, trackerCount);
+        ++trackerCount;
     }
 
     function supportsInterface(bytes4 interfaceId)
