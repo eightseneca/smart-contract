a28e35523e576a6c9e64953dc944ea97444cda8a
hoangphuc <hoangphucnb97@gmail.com>
2022-12-09 15:02:24 +0700

    Fix bug and add test upload new file

--- test/DocumentTrackerContract.ts
+++ test/DocumentTrackerContract.ts
@@ -2,6 +2,8 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers, upgrades } from "hardhat";
 import { expect } from 'chai';
 import { DocumentTrackerContract } from "../typechain-types";
+import { getHashContentOfFile } from "./utils";
+import { extendEnvironment } from "hardhat/config";
 
 context(`DocumentTrackerContract`, async () => {
     let documentTracker: DocumentTrackerContract;
@@ -16,5 +18,20 @@ context(`DocumentTrackerContract`, async () => {
 
     it(`Deploy success`, async () => {
         expect(documentTracker.address).to.be.properAddress;
+    })
+
+    it(`Mint new content file`, async () => {
+        const hashFileA = getHashContentOfFile('fileA.txt');
+        await documentTracker.connect(account1).mint(hashFileA);
+        const balanceOfAccount1 = await documentTracker.balanceOf(account1.address);
+        expect(balanceOfAccount1).to.be.equal(1);
+
+        const hashIndex = await documentTracker.contentIndex(hashFileA);
+        expect(hashIndex.isExist).to.be.equal(true);
+        expect(hashIndex.tokenId).to.be.equal(0);
+
+        const trackerContent = await documentTracker.trackers(0);
+        expect(trackerContent.hashContent).to.be.equal(hashFileA);
+        expect(trackerContent.orgOwner).to.be.equal(account1.address);
     })
 })
--- test/utils.ts
+++ test/utils.ts
@@ -3,6 +3,6 @@ import { ethers } from "hardhat";
 import path from "path";
 
 export function getHashContentOfFile(fileName: string): string {
-    const content = fs.readFileSync(path.join('./resources', fileName), 'binary');
-    return ethers.utils.keccak256(content);
+    const content = fs.readFileSync(path.join(__dirname, './resources', fileName), 'utf8');
+    return ethers.utils.keccak256(Buffer.from(content));
 }
